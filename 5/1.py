a9e822d5879e541405f9faadca1174dbd6e82281
GrandSAURON <77923565+GrandSAURON@users.noreply.github.com>
2021-10-06 19:30:47 +0300

    Create 1.py

---
+++ 5/1.py
@@ -0,0 +1,21 @@
+def bin_to_dec(n):
+    return(int(n, base=2))
+
+def dec_to_bin(n):
+    a = ""
+    while n > 0:
+        k = n % 2
+        a = str(k) + a
+        n = n // 2
+    return a
+
+def process(n):
+    n = dec_to_bin(n)
+    if int(n) % 2 == 0:
+        n += "01"
+    else:
+        n+= "10"
+    return bin_to_dec(n)
+        
+for i in range(1, 100):
+    print(i, process(i))
