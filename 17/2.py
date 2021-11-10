19a5b0a996dff93d7343b22a55a67a3e18d19e6b
GrandSAURON <77923565+GrandSAURON@users.noreply.github.com>
2021-11-10 19:40:41 +0300

    Create 2.py

---
+++ 17/2.py
@@ -0,0 +1,12 @@
+f = open("17.txt", "r")
+s = list(map(int, f.read().split()))
+counter = 0
+max_sum = 0
+for i in range(len(s)-1):
+    for k in range(i+1, len(s)):
+        if (s[i] + s[k]) % 9 == 0:
+            counter +=1
+            max_sum = max(max_sum, s[i] + s[k])
+       
+print(counter, max_sum)
+# 9 9
