ab3a065aaa3a3c1b47984955e346452f93f635a0
GrandSAURON <77923565+GrandSAURON@users.noreply.github.com>
2021-12-01 19:15:53 +0300

    Create 3.py

---
+++ 17/3.py
@@ -0,0 +1,18 @@
+f = open("17.txt", "r")
+lines = f.readlines()
+count = 0
+
+def get_sum(n: str):
+    ans = 0
+    for el in n:
+        ans += int(el)
+    return ans
+
+cnt = 0
+max_val = 0
+for num in lines:
+    if (get_sum(num.strip('\n')) % 3 == 0):
+        cnt += 1
+        max_val = max(max_val, int(num))
+
+print(cnt, max_val)
